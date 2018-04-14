e7ad3374ca6435f52ac5f4743ca46bff56888696
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-14 15:27:09 +0800

    Added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
+import { AppComponent } from './app.component'
+import { AboutComponent } from './components/about/about.component'
+import { PortfolioComponent } from './components/portfolio/portfolio.component'
+import { ContactComponent } from './components/contact/contact.component'
 
-import { AppComponent } from './app.component';
-import { AboutComponent } from './components/about/about.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ContactComponent } from './components/contact/contact.component';
-
+const routes: Routes = [
+  {
+    path: '',
+    component: AboutComponent
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +29,8 @@ import { ContactComponent } from './components/contact/contact.component';
     PortfolioComponent,
     ContactComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
