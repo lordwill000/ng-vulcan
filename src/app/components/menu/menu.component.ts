a0254698adf93020af0e3d816b637dc6e42b50af
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-14 18:51:18 +0800

    Updated menu

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.sass']
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  openMenu() {
+    document.querySelector('.menu-overlay').classList.toggle('active-menu')
+    document.querySelector('.hamburger').classList.toggle('active-hamburg')
+    const menuItems = document.querySelectorAll('.menu-item')
+    Array.from(menuItems).forEach((e, i) => {
+      setTimeout(() => {
+        e.classList.toggle('fade-in-left')
+      }, i * 250 + 300)
+    })
   }
-
 }
