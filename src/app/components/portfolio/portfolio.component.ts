ac674dd99306761767d64039ee61361c34cfbdc1
lordcndy000 <lordwill.mabalot@gmail.com>
2018-04-15 18:47:29 +0800

    Added animation in portfolio

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -11,9 +11,25 @@ export class PortfolioComponent implements OnInit {
   constructor(private appService: AppService) {}
 
   ngOnInit() {
-    this.appService.fetchPortfolio().subscribe(data => {
-      this.portfolioData = data
-      console.log(this.portfolioData)
+    this.appService.fetchPortfolio().subscribe(
+      data => {
+        this.portfolioData = data
+      },
+      error => console.log(error),
+      () => {
+        setTimeout(() => {
+          this.showPortfolio()
+        }, 100)
+      }
+    )
+  }
+
+  showPortfolio() {
+    const singlePortfolio = document.querySelectorAll('.portfolio-single')
+    Array.from(singlePortfolio).forEach((e, i) => {
+      setTimeout(() => {
+        return e.classList.add('animate-in')
+      }, i * 250 + 400)
     })
   }
 }
